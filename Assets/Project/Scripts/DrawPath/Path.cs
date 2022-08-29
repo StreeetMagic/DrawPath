6b76ba6a7b15df3a312caf8527359eb8c1776aae
Vlad Parastaev <streeetmagic@gmail.com>
2022-08-29 16:25:28 +0300

    Ноль прогресса

--- Assets/Project/Scripts/DrawPath/Path.cs
+++ Assets/Project/Scripts/DrawPath/Path.cs
@@ -13,6 +13,32 @@ namespace Scripts.DrawPath
 
         private readonly List<MainPoint> _points = new List<MainPoint>();
 
+        public void AddMainPoint(Vector3 location)
+        {
+            var point = Instantiate(_mainPointTemplate, location, Quaternion.identity, _pointContainer.transform);
+
+            _points.Add(point);
+
+            if (_points.Count == 2)
+            {
+                 TurnToParent(_points[1], _points[0]);
+                _points[0].InitSidePoints();
+                _points[1].InitSidePoints();
+                //_points[0].transform.forward = _points[1].transform.forward;
+            }
+
+            if (_points.Count > 2)
+            {
+                var lastNumber = _points.Count - 1;
+
+                point.transform.rotation = _points[lastNumber - 1].transform.rotation;
+                TurnToParent(_points[lastNumber], _points[lastNumber - 1]);
+                point.transform.right = Vector3.forward;
+                _points[lastNumber].InitSidePoints();
+                CreateCube(_points[lastNumber - 1], _points[lastNumber]);
+            }
+        }
+
         private void CreateCube(MainPoint start, MainPoint finish)
         {
             var cube = Instantiate(_cubeTemplate, transform.position, Quaternion.identity,
@@ -23,35 +49,11 @@ namespace Scripts.DrawPath
             cube.UpdateMesh();
         }
 
-        private void TurnToParent(MainPoint ascendant, MainPoint parent)
+        private void TurnToParent(MainPoint child, MainPoint parent)
         {
-            var transform1 = ascendant.transform;
-            Vector3 direction = parent.transform.position - transform1.position;
-            transform1.forward = direction;
-            transform1.eulerAngles = new Vector3(transform1.eulerAngles.x, 90, 0);
-        }
-
-        public void AddMainPoint(Vector3 location)
-        {
-            var point = Instantiate(_mainPointTemplate, location, Quaternion.identity, _pointContainer.transform);
-
-            _points.Add(point);
-
-            if (_points.Count == 2)
-            {
-                _points[0].InitSidePoints();
-                _points[1].InitSidePoints();
-                TurnToParent(_points[1], _points[0]);
-                _points[0].transform.forward = _points[1].transform.forward;
-            }
-
-            if (_points.Count > 2)
-            {
-                var number = _points.Count - 1;
-                _points[number].InitSidePoints();
-                TurnToParent(_points[number], _points[number - 1]);
-                CreateCube(_points[number - 1], _points[number]);
-            }
+            var direction = parent.transform.position - child.transform.position;
+            child.transform.forward = direction;
+            // child.transform.eulerAngles = new Vector3(child.transform.eulerAngles.x, 90, 0);
         }
     }
 }
--- Assets/Project/Scripts/Player/Player.cs
+++ Assets/Project/Scripts/Player/Player.cs
@@ -5,10 +5,11 @@ namespace Scripts.Player
     public class Player : MonoBehaviour
     {
         [SerializeField] private Rigidbody _pelvis;
-        
+
         private void Start()
         {
             //_pelvis.AddTorque(new Vector3(0,100,100));        
+
         }
     }
 }
