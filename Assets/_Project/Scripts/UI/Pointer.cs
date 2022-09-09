abdb17cd349891d2291db6e7be761c879917aab0
Vlad Parastaev <streeetmagic@gmail.com>
2022-09-09 11:47:14 +0300

    Pointer

--- Assets/_Project/Scripts/UI/Pointer.cs
+++ Assets/_Project/Scripts/UI/Pointer.cs
@@ -1,14 +1,16 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Scripts.UI
 {
-    [ExecuteAlways]
+    //[ExecuteAlways]
     public class Pointer : MonoBehaviour
     {
         [SerializeField] private Transform _playerTransform;
-        [SerializeField] private Transform _worldPointer;
+        //[SerializeField] private Transform _worldPointer;
         [SerializeField] private Camera _camera;
         [SerializeField] private Transform _pointerIconTransform;
+        [SerializeField] private Image _image;
 
         private void Update()
         {
@@ -33,11 +35,21 @@ namespace Scripts.UI
                 }
             }
 
+            if (minDistance > direction.magnitude)
+            {
+                _image.gameObject.SetActive(false);
+            }
+            else
+            {
+                _image.gameObject.SetActive(true);
+            }
+            
             minDistance = Mathf.Clamp(minDistance,0, direction.magnitude);        
             var worldPosition = ray.GetPoint(minDistance);
-            _worldPointer.position = worldPosition;
+           // _worldPointer.position = worldPosition;
 
             _pointerIconTransform.position = _camera.WorldToScreenPoint(worldPosition);
+          //  _pointerIconTransform.forward = -direction;
         }
     }
 }
