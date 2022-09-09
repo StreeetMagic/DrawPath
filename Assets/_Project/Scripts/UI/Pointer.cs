6e21a74e793b1cf5c57abbde5ea174faca27980f
Vlad Parastaev <streeetmagic@gmail.com>
2022-09-09 13:38:52 +0300

    YellowArrow

--- Assets/_Project/Scripts/UI/Pointer.cs
+++ Assets/_Project/Scripts/UI/Pointer.cs
@@ -1,29 +1,45 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Scripts.UI
 {
-    //[ExecuteAlways]
+    [ExecuteAlways]
     public class Pointer : MonoBehaviour
     {
         [SerializeField] private Transform _playerTransform;
-        //[SerializeField] private Transform _worldPointer;
         [SerializeField] private Camera _camera;
         [SerializeField] private Transform _pointerIconTransform;
         [SerializeField] private Image _image;
 
+        [SerializeField] private TextMeshProUGUI _text;
+        [SerializeField] private Vector3 _offcet = new Vector3(5, 0, 0);
+
         private void Update()
+        {
+            LocateArrow();
+            LocateText();
+        }
+
+        private void LocateText()
+        {
+            _text.transform.position = _image.transform.position + _offcet;
+
+            var distance = Mathf.Round((transform.position - _playerTransform.position).magnitude);
+
+            var text = distance.ToString() + " m";
+
+            _text.text = text;
+        }
+
+        private void LocateArrow()
         {
             var position = _playerTransform.position;
             var direction = transform.position - position;
-            Debug.DrawRay(position, direction);
-
             var ray = new Ray(position, direction);
-
             var planes = GeometryUtility.CalculateFrustumPlanes(_camera);
-
             var minDistance = Mathf.Infinity;
-            
+
             for (var i = 0; i < planes.Length; i++)
             {
                 if (planes[i].Raycast(ray, out var distance))
@@ -43,13 +59,13 @@ namespace Scripts.UI
             {
                 _image.gameObject.SetActive(true);
             }
-            
-            minDistance = Mathf.Clamp(minDistance,0, direction.magnitude);        
+
+            minDistance = Mathf.Clamp(minDistance, 0, direction.magnitude);
             var worldPosition = ray.GetPoint(minDistance);
-           // _worldPointer.position = worldPosition;
 
             _pointerIconTransform.position = _camera.WorldToScreenPoint(worldPosition);
-          //  _pointerIconTransform.forward = -direction;
+
+            _pointerIconTransform.up = direction;
         }
     }
 }
